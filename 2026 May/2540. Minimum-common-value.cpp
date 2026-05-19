c45bb1404aa3f96513d1d1e4c598cc89a6d4288c
middoly-ep <middolynotclone@gmail.com>
2026-05-19 08:29:24 +0700

    19th May 2026 daily challenge

---
+++ 2026 May/2540. Minimum-common-value.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        int i = 0;
+        int j = 0;
+        while (i < m && j < n){
+            if (nums1[i] == nums2[j]) return nums1[i];
+            if (nums1[i] > nums2[j]) j++;
+            else i++;
+        }
+
+        return -1;
+    }
+};
