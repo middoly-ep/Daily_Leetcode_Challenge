5a544368b3ec38a71f67a60536c9d7862a26b1a5
middoly-ep <middolynotclone@gmail.com>
2026-06-18 01:31:35 +0700

    18th June 2026 daily challenge

---
+++ 2026 June/3614. Process-string-with-special-operations-II.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <stack>
+
+using namespace std; 
+
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        stack<long long> st;
+        st.push(0LL);
+        long long curr = 0;
+        for (char x : s){
+            if (x == '*'){
+                curr = max(curr - 1, 0LL);
+                st.push(curr);
+            }
+            else if (x == '#'){
+                curr *= 2;
+                st.push(curr);
+            }
+            else if (x == '%'){
+                st.push(curr);
+            }
+            else{
+                curr++;
+                st.push(curr);
+            }
+        }
+        if (k >= st.top()) return '.';
+
+        int pos = s.size() - 1;
+        while (pos >= 0){
+            if (s[pos] == '#'){
+                st.pop();
+                if (k >= st.top()) k -= st.top();
+            }
+            else if (s[pos] == '*'){
+                st.pop();
+            }
+            else if (s[pos] == '%'){
+                k = st.top() - 1 - k;
+                st.pop();
+            }
+            else{
+                if (k == st.top() - 1) return s[pos];
+                st.pop();
+            }
+            pos--;
+        }
+        return '.';
+    }
+};
