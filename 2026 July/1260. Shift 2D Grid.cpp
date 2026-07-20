b6f29a5a27b3406103fda70af8ec138aa203e9a7
middoly-ep <middolynotclone@gmail.com>
2026-07-20 23:10:15 +0700

    20th July 2026 daily challenge

---
+++ 2026 July/1260. Shift 2D Grid.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> res(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            int idx = (i * n + j + k) % (m * n);
+            res[idx / n][idx % n] = grid[i][j];
+        }
+        }
+        return res;
+    }
+};
