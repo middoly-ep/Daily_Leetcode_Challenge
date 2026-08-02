8eb5c37f73eb90bb2e1e92b5da074cd3243aa9d4
middoly-ep <middolynotclone@gmail.com>
2026-08-02 20:20:42 +0700

    2nd August 2026 daily challenge

---
+++ 2026 July/877. Stone Game.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
+//Given that there is always an even number of piles and odd number of stones, Alice can always choose all even indices or all odd indices, so Alice always win by choosing the half with more stone
